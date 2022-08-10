85b2847fe0834e793aacc4893f52057348d01074
Vitaliy <vitaliy.prihotsky@gmail.com>
2022-08-10 14:25:31 +0300

    modified:   my-app/package.json 	modified:   my-app/src/app/app.module.ts 	modified:   my-app/src/app/core/core.module.ts

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './core/main-page/main-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { LoginComponent } from './core/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CoreModule } from './core/core.module';
 
@@ -18,8 +14,6 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatFormFieldModule,
     FormsModule,
     ReactiveFormsModule,
     CoreModule,
